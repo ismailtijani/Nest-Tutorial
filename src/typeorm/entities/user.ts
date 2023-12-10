a0a4e38aa5881b4b1bbea9ff4d19fd38ff07f14c
Ismail Tijani <ismailtijani10@yahoo.com>
2023-12-10 21:27:46 +0100

    Implementing TypeORM Entities

---
+++ src/typeorm/entities/user.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ nullable: false })
+  firstName: string;
+
+  @Column({ nullable: false })
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column()
+  createdAt: Date;
+}
