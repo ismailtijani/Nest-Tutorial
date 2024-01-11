7ed5875d95dafe7ae48a5cb7fbe3a227c76cfd81
Ismail Tijani <ismailtijani10@yahoo.com>
2024-01-11 15:47:38 +0100

    Implementing Validation to the user dto

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,12 +1,17 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class createUserDto {
   @IsNotEmpty()
+  @MinLength(3)
   firstName: string;
+
   @IsNotEmpty()
   @IsString()
+  @MinLength(3)
   lastName: string;
+
   @IsEmail()
+  @MinLength(8)
   email: string;
   password: string;
 }
