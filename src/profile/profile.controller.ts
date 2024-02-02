80470ee1ebcd0799d0d7399f0b9ada797c17af1d
Ismail Tijani <ismailtijani10@yahoo.com>
2024-02-02 23:35:45 +0100

    code refactoring

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -15,7 +15,7 @@ import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 
-@Controller(':id/profile')
+@Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
